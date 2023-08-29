322c316183e082f31c6828af6491b76ca0e03335
Duncan Freeman <dfreeman@lucidyne.com>
2023-08-29 10:35:27 -0700

    Fix gradient probably

--- src/app.rs
+++ src/app.rs
@@ -840,15 +840,32 @@ fn grid_to_particles(particles: &mut [Particle], grid: &Array2D<GridCell>) {
         );
 
         fn gradient(p: GridPos, v: Vec2) -> Vec2 {
-            let p = v - index_to_pos(p);
+            let p = index_to_pos(p);
+            let f = v.fract();
 
-            let wd = |u: f32| if u > 0. { (1. - u, -1.0) } else { (u, 1.0) };
-
-            let (x_weight, x_deriv) = wd(p.x);
-            let (y_weight, y_deriv) = wd(p.y);
-
-            Vec2::new(y_weight * x_deriv, x_weight * y_deriv)
+            if v.x > p.x {
+                if v.y > p.y {
+                    Vec2::new(f.y - 1., f.x - 1.)
+                } else {
+                    Vec2::new(-f.y, 1. - f.x)
+                }
+            } else {
+                if v.y > p.y {
+                    Vec2::new(1. - f.y, -f.x)
+                } else {
+                    Vec2::new(f.y, f.x)
+                }
+            }
         }
+
+        /*
+        let v = Vec2::new(0.25, 0.15);
+        dbg!(gradient((0, 0), v));
+        dbg!(gradient((1, 0), v));
+        dbg!(gradient((0, 1), v));
+        dbg!(gradient((1, 1), v));
+        todo!();
+        */
 
         // Interpolate grid vectors
         part.deriv[0] = gather_vector(u_pos, |p| grid[p].vel.x * gradient(p, u_pos));
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
